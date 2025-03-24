394f68cd489820da027989e0f7cd6622fd115974
lexi.callyscoped <lexi.callyscoped@gmail.com>
2025-03-24 15:01:51 +0530

    feat: init aggregator

---
+++ aggregate.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+REPOS_FILE="repositories.txt"
+LOCK_FILE="repositories.lock"
+REPOS_DIR="repos"
+
+# PURGE!!
+rm -rf "$REPOS_DIR"
+mkdir -p "$REPOS_DIR"
+rm -f "$LOCK_FILE"
+
+# the `||` is for when there's no trailing new line, but we still have content in the last line
+while read -r path url branch || [[ -n "$path" ]]; do
+  # empty lines and comments allowed
+  [[ -z "$path" || "$path" == \#* ]] && continue
+
+  dir="$REPOS_DIR/$path"
+
+  echo "Cloning repository: $path"
+  mkdir -p "$dir"
+  git clone --quiet --progress --depth 1 --no-tags --branch "$branch" "$url" "$dir"
+
+  hash=$(git -C "$dir" rev-parse HEAD)
+  echo "$path $hash" >> "$LOCK_FILE"
+
+  rm -rf "$dir/.git"
+  echo # newline
+done < "$REPOS_FILE"
+
+last_update=$(git log -1 --format=%cd --date=short 2>/dev/null || echo "initial")
+current_date=$(date +%Y-%m-%d)
+commit_title="Update repositories: $last_update to $current_date"
+commit_body="$(git diff --word-diff HEAD -- "$LOCK_FILE" | tail -n +6)"
+
+git add .
+git commit -m "$commit_title" -m "$commit_body"
+git push origin HEAD
